3df3bf952614f95ede842e9069d532d969ac8765
takiafarhin <takiafarhin@iut-dhaka.edu>
2024-01-29 15:58:12 +0600

    algo

---
+++ lab_1/test.cpp
@@ -0,0 +1,26 @@
+//Takia Farhin - takia03
+//IUT, 2024
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Redirect input from input.txt
+    ifstream in("../input.txt"); 
+    cin.rdbuf(in.rdbuf());
+
+    // Redirect output to output.txt
+    ofstream out("../output.txt");
+    cout.rdbuf(out.rdbuf());
+
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cout << "Rayshad ekta Faltu" << endl;
+    }
+
+    
+    return 0;
+}
